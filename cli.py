c21841947a3b9dd835d27e31eec190db7af841cd
Rias <rtgaff3@gmail.com>
2025-05-23 00:50:50 -0400

    Developed a cli to be more user friendly and more reusable

--- cli.py
+++ cli.py
@@ -1,105 +1,79 @@
-from FlagEmbedding import BGEM3FlagModel # type: ignore
+import helpers
+import argparse
+import requests
 import json
+import time
 import os
-import numpy as np
-import pandas as pd
-import faiss
-from sklearn.metrics.pairwise import cosine_similarity
-useful_features = ["card_faces" , "cmc", "color_identity", "defense", "edhrec_rank", "game_changer", 
-                   "keywords",  "life_modifier", "loyalty", "mana_cost", "name", "oracle_text", 
-                   "power", "produced_mana", "toughness", "type_line", "rulings"]
+import re
+from helpers import get_embeddings, perform_search
 
 saved_embeddings = "Embedded_Magic_cards.pkl"
-redo_embeddings = True
 
+def get_scryfall_bulk_data(data_type="default_cards", output_dir="Data"):
+   bulk_data_url = "https://api.scryfall.com/bulk-data"
+   print(f"Fetching bulk data index from: {bulk_data_url}")
 
-#TODO # Need to be able to define the importance of words. 
-# For example we get a lot of cards that are similar because they share the same "name " for example all of the urzas are more similar than other cards
-# I want cards to similar soly their attributes. 
-# (It may be better for now to just do the oracle text and ignore everything else? But these you still ahve the problem where cards reference themselves and the model thinks that means they are similar)
-# Which yes they are however the name of the card should not matter. Only Some aspects of card text we care about
-def gen_text_cards(df):
-    samples = []
-    for card_name, row in df.iterrows():
-        row_strings = [f"{card_name};; "]
-        for column in df.columns:
-            if column != 'name':
-                value = row[column]
-                if value == '':
-                    continue
-                row_strings.append(f"{column}; {value}")
-        # Combine into single string
-        sample = f"{card_name}; " + ", ".join(row_strings)
-        samples.append(sample)
-    
-    return samples
+   try:
+      response = requests.get(bulk_data_url)
+      response.raise_for_status()
+      bulk_data_index = response.json()
 
-def cosine_similarity_search(df, card_name, all_embeddings):
-    if card_name not in df.index:
-        print(f"{card_name} is not a card present within dataframe")
-        return None
-    else:
-        target_embedding = np.array(df.loc[card_name, "embeddings"]).reshape(1, -1)
-        similarities = cosine_similarity(target_embedding, all_embeddings)[0]
+      for data_object in bulk_data_index.get("data", []):
+         download_url = data_object.get("download_uri")
+         data_type = data_object.get("type")
 
-        similarities_df = pd.DataFrame({'card_name': df.index,
-                                        'similarity': similarities})
-        similarities_df = similarities_df[similarities_df['card_name'] != card_name]
+         # All cards is two 2Gb so skip
+         if data_type == "all_cards":
+            continue
 
-        return similarities_df.sort_values(by='similarity', ascending=False)
+         print(f"Downloading '{data_type}': {download_url}")
+         os.makedirs(output_dir, exist_ok=True)
+         output_path = os.path.join(output_dir, re.sub(r'\d', '',os.path.basename(download_url)))
 
-def k_most_similar(search_term, df, k=5):
-    cos_search_results = cosine_similarity_search(df, search_term, np.stack(df["embeddings"].values))
-    if cos_search_results is not None:
-        print(f"Top {k} most similar cards to search")
-        print(cos_search_results.head(k))
-        for i in cos_search_results.head(k)["card_name"]:
-            print(i + ":", df.loc[i]["oracle_text"], df.loc[i])
+         print(f"Downloading bulk data to: {output_path}")
+         data_response = requests.get(download_url, stream=True)
+         data_response.raise_for_status()
+
+         with open(output_path, 'wb') as f:
+            for chunk in data_response.iter_content(chunk_size=8192):
+               f.write(chunk)
+         print(f"Successfully downloaded '{data_type}' bulk data to {output_path}\n{'=' * 60}\n")
+
+   except requests.exceptions.RequestException as e:
+      print(f"Network or HTTP error occurred: {e}")
+   except json.JSONDecodeError as e:
+      print(f"Error decoding JSON response: {e}")
+   except Exception as e:
+      print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
-    read_scryfall_data = False
-    redo_embeddings = False
-    search_term = "Urza, Lord High Artificer" 
-    # Read the oracle and rulings if saved or if user indicates
-    if not os.path.exists(saved_embeddings) or read_scryfall_data:
-        print(f"Building data frame and doing embeddings")
-        with open("Data/oracle-cards-20250425090226.json", "r") as file:
-            data = json.load(file)
-        with open("Data/rulings-20250425090033.json", "r") as file:
-            rulings = json.load(file)
+   parser = argparse.ArgumentParser(description= 'cli tool')
 
-        oracle_id_loc = {}
-        for i in range(len(data)):
-            oracle_id_loc[data[i]["oracle_id"]] = i
+   parser.add_argument('-u', '--update', action='store_true', help='Update the scryfall bulk data')
+   parser.add_argument('-e', '--embeddings', action='store_true', help='Redo the vector embeddings')
+   parser.add_argument('-s', '--search', type=str, help='Search through similar cards to the given card name')
+   parser.add_argument('-m', '--model', type=str, help='Model to use for vector embeddings. NOTE: This it is software is highly model dependent', default="paraphrase-MiniLM-L6-v2")
+   args = parser.parse_args()
+   if args.search is None and args.update is False and args.embeddings is False:
+      parser.print_help()
+      exit()
 
-        for r in rulings:
-            if 'rulings' not in data[oracle_id_loc[r["oracle_id"]]]:
-                data[oracle_id_loc[r["oracle_id"]]]['rulings'] = r['comment'] 
-            else: 
-                data[oracle_id_loc[r["oracle_id"]]]['rulings'] + " " + r['comment']
-        #Process the data
-        df = pd.DataFrame(data, columns=useful_features)
-        df = df[df['type_line'] != "Card // Card"]
-        df = df.fillna('')
-        df.set_index('name', inplace=True)
-    else:
-        print(f"Dataframe read from {saved_embeddings}")
-        df = pd.read_pickle(saved_embeddings)
+   print(args)
 
-    if redo_embeddings:
-        #Create Text card object to encode as sentences 
-        text_cards = gen_text_cards(df)
-        embedding_model = BGEM3FlagModel('BAAI/bge-m3') 
-        #card_embeddings = embedding_model.encode(df.index.tolist(), return_dense=True)
-        card_embeddings = embedding_model.encode(text_cards, return_dense=True)
-        print(card_embeddings)
-        print(df.shape, card_embeddings['dense_vecs'].shape)
-        print(card_embeddings['dense_vecs'][-1])
-        df['embeddings'] = card_embeddings['dense_vecs'].tolist()
-        df.to_pickle(saved_embeddings)
-        print(f"Build data frame and saved do {saved_embeddings}")
+   update_embeddings = args.embeddings
+   if args.update or os.path.exists('Data/oracle-cards-.json') is False:
+      # Update all bulk data
+      print("Updating bulk data...")
+      get_scryfall_bulk_data()
+      update_embeddings = True
 
-    #embeddings = np.array(df["embeddings"].tolist()).astype('float32')
-    #string_to_int_id = {str_id: i for i, str_id in enumerate(df.index)}
-
-    k_most_similar(search_term, df, 5)
+   if args.search:
+      # Redo embeddings since we have new data or if user asked
+      # Otherwise just fetched the stored dataframe
+      if update_embeddings:
+         print("Updating embeddings...")
+      else:
+         print("Getting embeddings from file")
+      df = get_embeddings(update_embeddings, model=args.model)
+      perform_search(df, search_vect=args.search)
---
+++ helpers.py
@@ -0,0 +1,130 @@
+from FlagEmbedding import BGEM3FlagModel # type: ignore
+import json
+import os
+import numpy as np
+import pandas as pd
+import faiss
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import re
+USED_FEATURES = ["card_faces" , "cmc", "color_identity", "defense", "edhrec_rank", "game_changer",
+                   "keywords",  "life_modifier", "loyalty", "mana_cost", "name", "oracle_text",
+                   "power", "produced_mana", "toughness", "type_line", "rulings"]
+
+#Downloads or returns the cached Sentence transformer model
+def download_model(model_name):
+    os.makedirs('local_models', exist_ok=True)
+    local_model_path = os.path.join(os.getcwd(), 'local_models', model_name)
+
+    try:
+        model = SentenceTransformer(model_name, cache_folder=local_model_path)
+        print(f"Cached and or loaded'{model_name}' from {local_model_path}\n")
+        return model
+    except Exception as e:
+        print(f"An error occurred during model download or loading: {e}")
+
+def get_embeddings(redo_embeddings=False, saved_embeddings="Embedded_Magic_cards.pkl", model="paraphrase-MiniLM-L6-v2"):
+    if redo_embeddings:
+        print(f"Building data frame and doing embeddings")
+        with open("Data/oracle-cards-.json", "r") as file:
+            data = json.load(file)
+        with open("Data/rulings-.json", "r") as file:
+            rulings = json.load(file)
+
+        oracle_id_loc = {}
+        for i in range(len(data)):
+            oracle_id_loc[data[i]["oracle_id"]] = i
+
+        for r in rulings:
+            if 'rulings' not in data[oracle_id_loc[r["oracle_id"]]]:
+                data[oracle_id_loc[r["oracle_id"]]]['rulings'] = r['comment']
+        # Process the data
+        df = pd.DataFrame(data, columns=USED_FEATURES)
+        df = df[df['type_line'] != "Card // Card"]
+        df = df.fillna('')
+        df.set_index('name', inplace=True)
+        # Create Text card object to encode as sentences
+        text_cards = gen_text_cards(df)
+
+        embedding_model = download_model(model)
+
+        # Encode the text cards to get embeddings
+        card_embeddings = embedding_model.encode(text_cards)
+
+        print(card_embeddings)
+        print(df.shape, card_embeddings.shape)
+        print(card_embeddings[-1])
+
+        df['embeddings'] = card_embeddings.tolist()
+        df.to_pickle(saved_embeddings)
+        print(f"Build data frame and saved to {saved_embeddings}")
+    else:
+        print(f"Dataframe read from {saved_embeddings}")
+        df = pd.read_pickle(saved_embeddings)
+
+    return df
+
+def gen_text_cards(df, summarize=True):
+    samples = []
+    for card_name, row in df.iterrows():
+        # row_strings = [f"{card_name};; "]
+        row_strings = [f""]
+        for column in df.columns:
+            if column != 'name' or column != card_name:
+                value = row[column]
+                # if column == 'oracle_text':
+                #    #Replace the name on the card with [CARD_NAME]
+                #    if np.char.find(value, card_name) != -1:
+                #        value = np.char.replace(value, card_name, '[CARD_NAME]')
+
+                #    name_split = re.split(' |,', card_name)
+                #    if np.char.find(value, name_split[0]) != -1:
+                #        value = np.char.replace(value, card_name[0], '[CARD_NAME]')
+                if value == '':
+                    continue
+                row_strings.append(f"{column}; {value}")
+        # Combine into single string
+        # sample = f"{card_name}; " + ", ".join(row_strings)
+        sample = f"".join(row_strings)
+        samples.append(sample)
+
+    return samples
+
+# Better to compute as a matrix instead
+def single_soft_cosine(vAt, vBt):
+    vA = vAt.reshape(1, -1)
+    vB = vBt.reshape(1, -1)
+    s = cosine_similarity(vA,vB)[0]
+    neu = np.dot(vA, vB.reshape(-1,1)) * s
+    d1 = np.sqrt(np.dot(vA, vA.reshape(-1,1))*s)
+    d2 = np.sqrt(np.dot(vB, vB.reshape(-1,1))*s)
+    return neu/np.dot(d1, d2.reshape(-1,1))
+
+def soft_cosine(target, all_embeddings):
+    return [single_soft_cosine(target, e) for e in all_embeddings]
+
+def cosine_similarity_search(df, card_name, all_embeddings, soft = True):
+    if card_name not in df.index:
+        print(f"{card_name} is not a card present within dataframe")
+        return None
+    else:
+        target_embedding = np.array(df.loc[card_name, "embeddings"]).reshape(1, -1)
+        if not soft:
+            similarities = cosine_similarity(target_embedding, all_embeddings)[0]
+        else:
+            similarities = soft_cosine(target_embedding, all_embeddings)
+
+        similarities_df = pd.DataFrame({'card_name': df.index, 'similarity': similarities})
+        similarities_df = similarities_df[similarities_df['card_name'] != card_name]
+        return similarities_df.sort_values(by='similarity', ascending=False)
+
+def perform_search(df, search_vect="", n=10, all_embeddings = None):
+    if all_embeddings is None:
+        all_embeddings = np.stack(df["embeddings"].values)
+
+    cos_search_results = cosine_similarity_search(df, search_vect, all_embeddings)
+    if cos_search_results is not None:
+        print(f"Top {n} most similar cards to search")
+        print(cos_search_results.head(n))
+        for k in cos_search_results.head(n)["card_name"]:
+            print(k + ":\n", df.loc[k]["oracle_text"], df.loc[k])

916f0b242d07befe608127fef834c02e7285bff8
Rias <rtgaff3@gmail.com>
2025-05-23 22:56:07 -0400

    Quick OCR test. Does not seem to be the way to go because of the nature of magic cards. Besides Secret layer cards may proove to be an issue. To get the ocr to work, I can focus on the set letters at the bottom right, the card number, and the card name

--- cardReader.py
+++ cardReader.py
@@ -1,5 +1,58 @@
+import cv2
+import pytesseract
+import argparse
+import sys
+import time
+
+ESC_KEY_CODE = 27
+DEFAULT_TIMEOUT = 5
+def extract_card_text(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
+#OCR does not seem to be the way to go
+def preprocess_card(img):
+    image = cv2.imread(img)
+    #Convert to greyscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 30, 150)
+    return edged
+
+def close_window(window_name):
+    """Handle the cleanup of an OpenCV window."""
+    cv2.destroyAllWindows()
+    print(f"{window_name} window closed")
+
+
+def show_card(card_image, timeout=DEFAULT_TIMEOUT):
+    window_name = "Card"
+    start_time = time.time()
+    cv2.imshow(window_name, card_image)
+
+    while True:
+        if cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
+            close_window(window_name)
+            break
+
+        if cv2.waitKey(1) & 0xFF == ESC_KEY_CODE:  # ESC key pressed
+            print("User closed the card window with ESC key")
+            close_window(window_name)
+            break
+
+        if time.time() - start_time > timeout:  # Timeout exceeded
+            print("Card window timed out")
+            close_window(window_name)
+            break
+
 #Read image of card
-
 #Based on printing of card
-
 #Setting to also just query card data via OCR from saved card database
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Image processing')
+    parser.add_argument('image_path', type=str, help='Card to process')
+    args = parser.parse_args()
+    card = preprocess_card(args.image_path)
+    show_card(card)
+    print(extract_card_text(card))
+    sys.exit(0)

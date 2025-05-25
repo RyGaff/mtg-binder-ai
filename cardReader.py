21b51f785467f643885b69af2a54e3762e7fc706
Rias <rtgaff3@gmail.com>
2025-05-25 15:42:24 -0400

    Improved OCR and query reliablity

--- cardReader.py
+++ cardReader.py
@@ -24,30 +24,52 @@ def extract_card_text(img):
     return text.lower()
 
 def find_roi_bottom_left(image, scale_factor=2):
+    # Todo Algorithmically format the card
+    # Search for largest contour in image
+    # Warp the image such that it is right side up and cropped properly
+    # Proceed with the following
     height, width, _ = image.shape
-    roi = image[int(height*0.93):int(height*0.97), int(width*0.05):int(width*0.187)]
+    roi = image[int(height*0.93):int(height*0.98), int(width*0.05):int(width*0.187)]
     return  roi
 
-def preprocess_card(img):
-    return process_bottom_left(img)
-
-def process_bottom_left(img):
-    # Step 1: Load the image
+def preprocess_card(img, scale_factor=4):
+    # Todo Handle cards that are missing set id and card number
+    # Calculate new dimensions
     image = cv2.imread(img)
+    new_size = (int(image.shape[1] * scale_factor), int(image.shape[0] * scale_factor))
+    # Resize the image
+    enlarged_image = cv2.resize(image, new_size, interpolation=cv2.INTER_CUBIC)
+    return process_bottom_left(enlarged_image)
+
+def process_bottom_left(image):
+    #Load the iamge
     cv2.imwrite("step_1_original.png", image)  # Save the original image for reference
 
+    #Crop to find set id and card number
     image = find_roi_bottom_left(image)
 
+    # Clean up image
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imwrite("step_2_grayscale.png", gray)  # Grayscale removes color information, focusing on intensity
     print("Gray", extract_card_text(gray))
 
+    gauss = cv2.GaussianBlur(gray, (5, 5), 0)
+
     kernel = np.ones((1, 1), np.uint8)
-    noise_removal = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+    noise_removal = cv2.morphologyEx(gauss, cv2.MORPH_OPEN, kernel)
     cv2.imwrite("step_3_noise_removal.png", noise_removal)
     print("Noise", extract_card_text(noise_removal))
 
-    return noise_removal
+    threshold = cv2.threshold(noise_removal, 50, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    #cv2.imwrite("step_4_threshold.png", noise_removal)
+    #print("Thresh", extract_card_text(threshold))
+    #threshold = cv2.adaptiveThreshold(noise_removal, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_OTSU, 5, 3)
+    # Save and display the results
+    cv2.imwrite("thresholded_image.png", threshold)
+
+    # Todo. Handle different lighting from images in world
+
+    return threshold
 
 def close_window(window_name):
     """Handle the cleanup of an OpenCV window."""
@@ -74,30 +96,43 @@ def show_card(card_image, timeout=DEFAULT_TIMEOUT):
             close_window(window_name)
             break
 
-def query_scryfall(cn = None, set = None, card_name = None):
+def query_scryfall(cn = None, set_id = None, card_name = None):
     if card_name is not None:
         response = requests.get(f"https://api.scryfall.com/cards/named?fuzzy={card_name}")
     else:
         cn = cn.strip()
-        set = set.strip()
-        print(">>>", cn, set, "<<<")
-        response = requests.get(f"https://api.scryfall.com/cards/search?q=set%3A{set}+cn%3A{cn}")
+        set_id = set_id.strip()
+        print(f"using set_id id {set_id} and using card number {cn}")
+        response = requests.get(f"https://api.scryfall.com/cards/search?q=set%3A{set_id}+cn%3A{cn}")
         print(response.json())
     response.raise_for_status()
     return response.json()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Image processing')
     parser.add_argument('image_path', type=str, help='Card to process')
     args = parser.parse_args()
     card = preprocess_card(args.image_path)
-    extracted_text = re.split(r"[ \-\n]", extract_card_text(card))
+    extracted_text = re.split(r"[ \W+]", extract_card_text(card))
     #show_card(card)
     print(extracted_text)
-    # Search up scryfall for the card
-    query_scryfall(cn=extracted_text[1], set=extracted_text[2])
+    # Loop through to make more reliable
+    card_id = ""
+    set_id = ""
+    for i in range(len(extracted_text)):
+        # Find card_id - find numeric number or some division operator
+        if extracted_text[i].isnumeric() and card_id == "":
+            card_id = extracted_text[i]
+        if '/' in extracted_text[i] and card_id == "":
+            card_id = extracted_text[i].split('/')[0]
+        # Find set_id - find 3 character code
+        if len(extracted_text[i]) == 3 and set_id == "":
+            set_id = extracted_text[i]
 
-    # If offline use saved data
+        # They should be the first returned if ocr read properly
+        if card_id != "" and set_id != "":
+            break
 
-    # If online query
-
+    print(card_id, set_id)
+    query_scryfall(cn=card_id, set_id=set_id)
     sys.exit(0)
